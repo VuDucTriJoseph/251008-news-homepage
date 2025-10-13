8f495824463a03d125888301327ad65e9e488e4c
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-10-13 16:05:01 +0700

    styling 90%

--- components/artical.jsx
+++ components/artical.jsx
@@ -3,10 +3,11 @@ function Artical({ articalData }) {
     <article>
       <img src={articalData.imgLink} alt={`image for ${articalData.title}`} />
       <h2>{articalData.order}</h2>
+
       <h3>
         <a href="#">{articalData.title}</a>
-        <p>{articalData.interpret}</p>
       </h3>
+      <p>{articalData.interpret}</p>
     </article>
   );
 }
