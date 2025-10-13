b51b14b16d4a1bdc4a0a779bc709537cb3e8d9c7
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-10-13 12:05:16 +0700

    styling: news 80%

--- components/firstNew.jsx
+++ components/firstNew.jsx
@@ -3,12 +3,16 @@ function FirstNew({ firstNewInfo }) {
   return (
     <div className="first-new">
       <picture>
-        <source media="(min-width:400px)" srcSet={firstNewInfo.imgLink} />
-        <img src={firstNewInfo.imgLinkMobile} alt="image first new" />
+        <source media="(min-width:600px)" srcSet={firstNewInfo.imgLink} />
+        <img
+          className="img"
+          src={firstNewInfo.imgLinkMobile}
+          alt="image first new"
+        />
       </picture>
       <h1>{firstNewInfo.title}</h1>
       <p>{firstNewInfo.interpret}</p>
-      <button>Read more</button>
+      <button className="btn-sec">Read more</button>
     </div>
   );
 }
--- components/news.jsx
+++ components/news.jsx
@@ -3,20 +3,22 @@ import New from "./new";
 function News({ newsInfo }) {
   // console.log(typeof newsInfo, newsInfo);
   return (
-    <table className="news">
-      <thead>
-        <tr>
-          <th>New</th>
-        </tr>
-      </thead>
-      <tbody>
-        {newsInfo.map((info, index) => (
-          <tr key={index}>
-            <New newInfo={info} />
+    <div className="news">
+      <table className="news">
+        <thead>
+          <tr>
+            <th>New</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {newsInfo.map((info, index) => (
+            <tr key={index}>
+              <New newInfo={info} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
